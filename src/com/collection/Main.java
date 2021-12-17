736c7974949ee1228e937a719b4fe88d7f5307d5
vladis18 <vladisbabayan@gmail.com>
2021-12-16 19:27:49 -0500

    Zaebali

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -3,7 +3,7 @@ package com.collection;
 public class Main {
     public static void main(String[] args) {
         for (int i=0;i<=5;i++){
-            System.out.println("I love you!!!");
+            System.out.println("I love you!!!!!");
         }
     }
 }
